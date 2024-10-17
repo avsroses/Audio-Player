4bc0093aaa044ee61819ce17eaeb53dc1c4970c1
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-17 11:18:38 +0100

    create audio player and add pause play function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // Create the audio player object
 const audioPlayer = new Audio();
+
+// Select play pause button element
+const playPauseButton = document.getElementById("play-button");
+
+// audioPlayer.src is the first song of the audio player by default
+audioPlayer.src = "assets/sound/Angeleyes.mp3";
+
+/**
+ * If audio player is not playing -> play sound
+ * If audio player is playing -> do not play sound
+ */
+function onPlayPauseClick() {
+    audioPlayer.play();
+}
+
+// Link onclick event to the onPlayPauseClick button
+playButton.onclick = onPlayPauseClick;
